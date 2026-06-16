3333903411e88b7fe7204d358145b2db6b161db3
ShreehariN <shreeharin2003@gmail.com>
2026-06-16 12:27:56 +0530

    Switch backend database connection to bazaarind_db

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,13 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-# Update this line inside backend/app/database.py
 CLOUD_MONGO_URL = "mongodb+srv://bazaar_admin:BazaarPass2026@bazaarind-cluster.toys3k4.mongodb.net/?appName=BazaarInd-Cluster"
 
 # Environment router: looks for system overrides, defaults to Atlas cloud link
 MONGO_URL = os.getenv("MONGO_URL", CLOUD_MONGO_URL)
-DATABASE_NAME = "ecommerce"
+
+# --- CHANGE THIS LINE ---
+DATABASE_NAME = "bazaarind_db" 
 
 # Initialize the non-blocking asynchronous data client loop
 client = AsyncIOMotorClient(MONGO_URL)
